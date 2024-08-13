9e8b03b78b30481865adb211d6b3b2485cff46b7
865038530 <74188835+865038530@users.noreply.github.com>
2024-08-13 22:07:47 +0800

    Add files via upload

---
+++ test/java/com/example/wangjian/syslog1/SysLog1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wangjian.syslog1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysLog1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
